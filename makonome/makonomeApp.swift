26dc021301eda756559755adfe13d3b277716987
Miro <mirocosic@gmail.com>
2025-06-23 16:54:30 +0200

    Initial Commit

---
+++ makonome/makonomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  makonomeApp.swift
+//  makonome
+//
+//  Created by Miro on 23.06.2025..
+//
+
+import SwiftUI
+
+@main
+struct makonomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ makonomeTests/makonomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  makonomeTests.swift
+//  makonomeTests
+//
+//  Created by Miro on 23.06.2025..
+//
+
+import Testing
+@testable import makonome
+
+struct makonomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
